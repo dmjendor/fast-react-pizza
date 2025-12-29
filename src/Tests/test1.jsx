d3b12573ba15a1d5278618a60b52bf49240ae027
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-29 11:56:22 -0500

    Implement Redux Thunks with createAsyncThunk

---
+++ src/Tests/test1.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import './index.scss';
+
+const App = () => {
+  const [hours, setHours] = useState('00');
+  const [minutes, setMinutes] = useState('00');
+
+  function handleIncrementHours() {
+    setHours((prev) => {
+      const next = (Number(prev) + 1) % 24;
+      return next.toString().padStart(2, '0');
+    });
+  }
+  function handleDecrementHours() {
+    if (hours === '00') return;
+    setHours((prev) => {
+      const next = (Number(prev) - 1) % 24;
+      return next.toString().padStart(2, '0');
+    });
+  }
+  function handleIncrementMinutes() {
+    setMinutes((prev) => {
+      const next = (Number(prev) + 1) % 60;
+      if (next >= 59) {
+        handleIncrementHours();
+        return '00';
+      }
+      return next.toString().padStart(2, '0');
+    });
+  }
+  function handleDecrementMinutes() {
+    if (minutes === '00') return;
+    setMinutes((prev) => {
+      const next = (Number(prev) - 1) % 60;
+      return next.toString().padStart(2, '0');
+    });
+  }
+
+  return (
+    <div id="ClockUpdater" className="container">
+      <div className="row">
+        <button
+          id="hours-up-button"
+          className="btn btn-outline-primary col"
+          onClick={handleIncrementHours}
+        >
+          &uarr;
+        </button>
+
+        {/* Level 1: Add your code here */}
+        <button
+          id="minutes-up-button"
+          className="btn btn-outline-primary col"
+          onClick={handleIncrementMinutes}
+        >
+          &uarr;
+        </button>
+      </div>
+
+      <div className="row">
+        <div id="clock" className="badge badge-primary col">
+          {`${hours}:${minutes}`}
+        </div>
+      </div>
+
+      <div className="row">
+        <button
+          id="hours-down-button"
+          className="btn btn-outline-primary col"
+          onClick={handleDecrementHours}
+        >
+          &darr;
+        </button>
+
+        {/* Level 1: Add your code here */}
+        <button
+          id="minutes-down-button"
+          className="btn btn-outline-primary col"
+          onClick={handleDecrementMinutes}
+        >
+          &darr;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,49 +2,47 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
-import { getCart } from '../cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
+import { formatCurrency } from '../../utilities/helpers';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
-  const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector((state) => state.user.username);
-  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
+  const [withPriority, setWithPriority] = useState(false);
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+
+  const isSubmitting = navigation.state === 'submitting';
+  const isLoadingAddress = addressStatus === 'loading';
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  function handleFetchAddress(e) {
+    e.preventDefault();
+    dispatch(fetchAddress());
+  }
+
+  if (cart.length === 0) return <EmptyCart />;
   return (
     // <Form method="POST" action="/order/new"></Form>
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let&apos;s go!</h2>
-
       <Form method="POST">
         <div className="mb-5 flex grow flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -69,11 +67,28 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
-            <input type="text" className="input w-full" name="address" required />
+            <input
+              type="text"
+              className="input w-full"
+              name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
+              required
+            />
+            {addressStatus === 'error' && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">{errorAddress}</p>
+            )}
           </div>
+          {!position.latitude && !position.logituded && (
+            <span className="med:top-[5px] absolute right-[3px] z-10 sm:top-[3px]">
+              <Button type="small" onClick={handleFetchAddress} disabled={isLoadingAddress}>
+                Get Address
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-10 flex items-center gap-5">
@@ -90,10 +105,18 @@ function CreateOrder() {
           </label>
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-
+        <input
+          type="hidden"
+          name="position"
+          value={
+            position.longitude && position.latitude
+              ? `${position.latitude},${position.longitude}`
+              : ''
+          }
+        />
         <div>
-          <Button type="primary" disabled={isSubmitting}>
-            {isSubmitting ? 'Placing Order' : 'Order now'}
+          <Button type="primary" disabled={isSubmitting || isLoadingAddress}>
+            {isSubmitting ? 'Placing Order' : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -120,7 +143,7 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
-
+  store.dispatch(clearCart());
   // if order status is without error, then redirect to new order page
   return redirect(`/order/${newOrder.id}`);
 }
